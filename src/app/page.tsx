ae3b607f936fa560aac67347fb83b029d5c2eeff
Nandavamsi07 <nandavamsisingireddy@gmail.com>
2025-03-12 11:52:28 +0530

    Alumini_Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import {Footercomponent} from "./components/Footer";
+import {Footercomponent} from "./components/footer";
 import { StandOut } from "./components/stand-out";
-
-
-
-
 export default function Home() {
   return (
     <div>
-      
       <StandOut/>
       <Footercomponent/>
     </div>
-  ); 
+  );
 }
