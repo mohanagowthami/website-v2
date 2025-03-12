3a89ab259336de94718fb093439859ee8ce63ad6
Nandavamsi07 <nandavamsisingireddy@gmail.com>
2025-03-12 12:02:33 +0530

    Alumini Card

---
+++ src/Common/AluminiCard.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+
+
+const people = [
+  {
+    name: "Kishore Gundra",
+    course: "ECE",
+    college: "Ideal Engineering College",
+    company: "/Amazon.png",
+    image: "/Kishore_Gundra.jpg.webp"
+  },
+  {
+    name: "Harish Kollu",
+    course: "ECE",
+    college: "Krishna University College of Engineering",
+    company: "/Amazon.png",
+    image: "/Harish_Kollu.webp", 
+  },
+  {
+    name: "Siva Durga Prasad",
+    course: "CSE",
+    college: "Aditya Engineering College",
+    company: "/Amazon.png", 
+    image: "/Siva_Durga_Prasad.webp",  
+  },
+];
+
+const AluminiCard = () => {
+  return (
+    <div className="text-center py-10 bg-gray-100">
+      <h2 className="text-3xl font-bold">
+        Turn Your Dreams into Reality. <span className="text-blue-600">The Journey Starts Here!</span>
+      </h2>
+      <p className="text-gray-600 mt-2">Get insider tips from your seniors and gear up for your dream job!</p>
+      <div className="flex flex-wrap justify-center gap-6 mt-10">
+        {people.map((person, index) => (
+          <div key={index} className="bg-white p-6 rounded-lg shadow-md w-72 text-center">
+            <div className="flex flex-row gap-6">
+              <div className="mt-5">
+                <Image src={person.image} alt={person.name} width={50} height={50} className="mx-auto rounded-full" />
+              </div>
+              <div>
+                <h3 className="mt-4 text-sm font-semibold">{person.name}</h3>
+                <p className="text-gray-500">{person.course}</p>
+                <p className="text-gray-500">{person.college}</p>
+                <div className="mt-4 border-t pt-4"></div>
+                <Image src={person.company} alt="Company" width={100} height={30} className="mx-auto mt-auto" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex self-right mt-10 justify-center">
+      </div>
+    </div>
+  );
+};
+
+export default AluminiCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import AluminiCard from "@/Common/AluminiCard";
 import {Footercomponent} from "./components/footer";
 import { StandOut } from "./components/stand-out";
+import { RequestCallbackButton } from "@/Common";
+
+
 export default function Home() {
   return (
     <div>
+      <AluminiCard/>
+      <div className="flex self-right mt-1 justify-center ml-300">
+        <RequestCallbackButton /></div>
       <StandOut/>
       <Footercomponent/>
     </div>
