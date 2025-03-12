bb88ec6de4cb07a25671b73b5a554faff7d3cbbf
Harshith6767 <harshithv6767@gmail.com>
2025-03-12 12:24:03 +0530

    course_deatils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,19 @@ import AluminiCard from "@/Common/AluminiCard";
 import {Footercomponent} from "./components/footer";
 import { StandOut } from "./components/stand-out";
 import { RequestCallbackButton } from "@/Common";
+import Programs from "./components/programs";
 
 
 export default function Home() {
   return (
     <div>
+      <Programs/>
       <AluminiCard/>
       <div className="flex self-right mt-1 justify-center ml-300">
         <RequestCallbackButton /></div>
       <StandOut/>
       <Footercomponent/>
+      
     </div>
   );
 }
