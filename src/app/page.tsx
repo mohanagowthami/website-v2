13af2bb018701550d89fc58908b9ba4bb000da08
sravya5454 <sravyamogali16@gmail.com>
2025-03-12 12:17:38 +0530

    merged changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,16 @@
-<<<<<<< HEAD
-import { LandingSection, LeftRightSection, Header } from "./components/landing-section";
-=======
-import {Footercomponent} from "./components/Footer";
+import {Footercomponent} from "./components/footer";
 import { StandOut } from "./components/stand-out";
-
->>>>>>> 160f17a55f2f1a7d30bd5fe0cff0489e8b71596a
+import { LandingSection } from "./components/landing-section";
 
 
 
 export default function Home() {
   return (
     <div>
-      
-<<<<<<< HEAD
-      <Header/>
-      <LeftRightSection/>
-=======
+      <LandingSection/>
+    
       <StandOut/>
       <Footercomponent/>
->>>>>>> 160f17a55f2f1a7d30bd5fe0cff0489e8b71596a
     </div>
   ); 
 }
