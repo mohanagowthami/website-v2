b03e567c0394c1661d682cc1f65e177a0b76b723
chinni <sriganneswarigajula@gmail.com>
2025-03-29 00:55:52 -0800

    did changes in header,page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { StandOut } from "./components/stand-out";
 
 import { LandingSection } from "./components/landing-section";
 import { FAQs } from "./components/faqs";
-import ExpertTutors from "./components/ExpertTutors/ExpertTutors";
+
 import { TestimonalsSwiper } from "./components/testimonals";
 
 import Programs from "./components/courses/programs";
 import { Footercomponent } from "./components/footer";
 import { AluminiComponent } from "./components/placements";
+import { ExpertTutors } from "./components/experttutors";
+
 
 
 
