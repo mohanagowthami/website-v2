1fa12cefe956452bb971b487b8cad6585ac43182
Ravi Junnu <ssvkusumamungi@gmail.com>
2025-04-02 11:15:09 +0530

    merged changed components

---
+++ src/app/components/CourseCurriculum/CurriculumHeader.tsx
@@ -0,0 +1,14 @@
+export const CurriculumHeader = () => {
+    return (
+      <div className="w-full bg-white border-b border-gray-200 mb-20">
+        <div className="flex items-center justify-between px-6 py-6 mx-auto w-full max-w-7xl">
+          <header className="text-3xl font-bold text-[#175CD3]">
+            HYPERSONICC.
+          </header>
+          <button className="bg-[#175CD3] hover:bg-[#1E4FAD] text-white font-medium py-2 px-6 rounded-lg transition-colors duration-200 shadow-sm transform hover:scale-105 active:scale-95 cursor-pointer">
+            Enroll Now
+          </button>
+        </div>
+      </div>
+    )
+  }
