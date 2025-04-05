50fa7ef5fd2b5c1eed4f65ea8f0e18c8f4d6adf8
Estherranithota <thotaestherrani6@gmail.com>
2025-04-05 11:33:02 +0530

    commited changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,20 @@ import HelpButton from "../components/Helpbutton/HelpButton";
 
 import EnrollPage from "./EnrollNow";
 
+<<<<<<< Updated upstream
+=======
+import { LandingSection } from "./components/landing-section";
+import { FAQs } from "./components/faqs";
+
+import { TestimonalsSwiper } from "./components/testimonals";
+
+import Programs from "./components/courses/programs";
+import { Footercomponent } from "./components/Footer";
+import { AluminiComponent } from "./components/placements";
+import { ExpertTutors } from "./components/ExpertTutors";
+import EnrollPage from "./EnrollNow";
+
+>>>>>>> Stashed changes
 
 
 
