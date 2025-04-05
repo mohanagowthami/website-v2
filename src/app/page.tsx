46eb4fb77cf11e1e70b3f16486ac6fc3065ea5d7
sravya5454 <sravyamogali16@gmail.com>
2025-04-05 12:50:57 +0530

    commited changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,6 @@ import { AluminiComponent } from "../components/placements";
 import { ExpertTutors } from "../components/ExpertTutors";
 import HelpButton from "../components/Helpbutton/HelpButton";
 
-<<<<<<< HEAD
-import { TestimonalsSwiper } from "./components/testimonals";
-
-import Programs from "./components/courses/programs";
-import { Footercomponent } from "./components/footer";
-import { AluminiComponent } from "./components/placements";
-import { ExpertTutors } from "./components/ExpertTutors";
-=======
->>>>>>> 5e364b9d05af929c79f01a6b830210f7a3115a39
 
 
 
